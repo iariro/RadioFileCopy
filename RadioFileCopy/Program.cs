b2656ee872fd5c14beb24cef51f14465f99c6759
kumagai <iariro@gol.com>
2018-02-05 08:47:09 +0900

    例外捕捉追加

--- RadioFileCopy/Program.cs
+++ RadioFileCopy/Program.cs
@@ -14,37 +14,44 @@ namespace RadioFileCopy
                 return;
             }
 
-            FileInfo [] sourceFiles = new DirectoryInfo(args[0]).GetFiles();
-            FileInfo[] destFiles = new DirectoryInfo(args[1]).GetFiles();
-            Array.Sort<FileInfo>(
-                sourceFiles,
-                delegate(FileInfo a, FileInfo b)
-                {
-                    // ファイル名でソート
-                    return a.Name.CompareTo(b.Name);
-                });
-
-            for (int i = sourceFiles.Length - 1; i >= 0; i--)
+            try
             {
-                bool find = false;
-                for (int j = 0; j < destFiles.Length; j++)
-                {
-                    if (sourceFiles[i].Name == destFiles[j].Name)
+                FileInfo[] sourceFiles = new DirectoryInfo(args[0]).GetFiles();
+                FileInfo[] destFiles = new DirectoryInfo(args[1]).GetFiles();
+                Array.Sort<FileInfo>(
+                    sourceFiles,
+                    delegate(FileInfo a, FileInfo b)
                     {
-                        find = true;
+                        // ファイル名でソート
+                        return a.Name.CompareTo(b.Name);
+                    });
+
+                for (int i = sourceFiles.Length - 1; i >= 0; i--)
+                {
+                    bool find = false;
+                    for (int j = 0; j < destFiles.Length; j++)
+                    {
+                        if (sourceFiles[i].Name == destFiles[j].Name)
+                        {
+                            find = true;
+                            break;
+                        }
+                    }
+
+                    if (!find)
+                    {
+                        Console.WriteLine(string.Format("Copying {0}", sourceFiles[i].Name));
+                        FileSystem.CopyFile(sourceFiles[i].FullName, Path.Combine(args[1], sourceFiles[i].Name));
+                    }
+                    else
+                    {
                         break;
                     }
                 }
-
-                if (!find)
-                {
-                    Console.WriteLine(string.Format("Copying {0}", sourceFiles[i].Name));
-                    FileSystem.CopyFile(sourceFiles[i].FullName, Path.Combine(args[1], sourceFiles[i].Name));
-                }
-                else
-                {
-                    break;
-                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
             }
         }
     }
