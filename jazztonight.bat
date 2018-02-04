7f2b8416fffac369eaf77c8d70709e284ca34942
kumagai <iariro@gol.com>
2018-02-05 08:44:30 +0900

    REMIX Z追加

--- jazztonight.bat
+++ jazztonight.bat
@@ -1,3 +1,4 @@
 
-RadioFileCopy\bin\Debug\RadioFileCopy E:\VOICE\NHK-FM D:\MUSIC\_Radio\RADIO
+RadioFileCopy\bin\Debug\RadioFileCopy "E:\VOICE\NHK-FM" D:\MUSIC\_Radio\RADIO
+RadioFileCopy\bin\Debug\RadioFileCopy "E:\VOICE\TOKYO FM" D:\MUSIC\_Radio\RADIO
 
