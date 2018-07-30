2459566e2796f1d9b3819ef886f058961f9b9660
kumagai <iariro@gol.com>
2018-07-30 09:02:04 +0900

    ドライブ自動判別 続き

--- jazztonight_remixz.bat
+++ jazztonight_remixz.bat
@@ -1,4 +1,4 @@
 
 RadioFileCopy\bin\Debug\RadioFileCopy VOICE\NHK-FM MUSIC\_Radio\RADIO
-RadioFileCopy\bin\Debug\RadioFileCopy VOICE\TOKYO FM MUSIC\_Radio\RADIO
+RadioFileCopy\bin\Debug\RadioFileCopy "VOICE\TOKYO FM" MUSIC\_Radio\RADIO
 
